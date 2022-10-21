4971ec33429d7f9423c36f2e930620385f1d4e41
Aleksandr <aleksandrignashov42@gmail.com>
2022-10-21 13:51:04 +0300

    fix adaptive and redeploy on firebase from heroku

--- src/redux/action/pizzas.js
+++ src/redux/action/pizzas.js
@@ -13,9 +13,11 @@ export const fetchPizzas = (sortBy, category) => (dispatch) => {
 
   axios
     .get(
-      `/pizzas?${
-        category !== null ? `category=${category}` : ""
-      }&_sort=${sortBy}&_order=desc`
+      // `/pizzas?${
+      //   category !== null ? `category=${category}` : ""
+      // }&_sort=${sortBy}&_order=desc`
+
+      "https://62cddbfda43bf780085fe7b3.mockapi.io/pizzas"
     )
     .then(({ data }) => {
       dispatch(setPizzas(data));
